c599fd7eea956aa95fb161d3e0d419a78fb5e177
Tal Etinger <talet@jfrog.com>
2025-12-24 10:43:05 +0200

    OPS-24 initial test

--- translate/app.py
+++ translate/app.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI(
     title="Translation Service",
-    description="A REST API service for English to French translation using Hugging Face models ",
+    description="A REST API service for English to French translation using Hugging Face models",
     version="1.0.0"
 )
 
